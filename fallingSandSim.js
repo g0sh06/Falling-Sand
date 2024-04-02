91ee246483959057deeb4e0b3f1ad89d502fa0cd
g0sh06 <g0sh06@github.com>
2024-04-02 13:31:39 +0200

    Falling one drop of sand

--- fallingSandSim.js
+++ fallingSandSim.js
@@ -4,21 +4,29 @@ let cols, rows; // Declare cols and rows globally
 
 // making 2d array
 function make2DArray(cols, rows) {
-    let array = Array(cols);
-    for (let i = 0; i < array.length; i++) {
-        array[i] = new Array(rows);
-        for (let j = 0; j < array[i].length; j++) {
-            array[i][j] = 0;
+    let arr = Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+        for (let j = 0; j < arr[i].length; j++) {
+            arr[i][j] = 0;
         }
     }
-    return array;
+    return arr;
 }
 
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(400, 400);
     cols = width / w; // Define cols
     rows = height / w; // Define rows
     grid = make2DArray(cols, rows); // Invoke make2DArray after cols and rows are defined
+
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
+        }
+    }
+    grid[10][10] = 1;
 }
 
 function draw() {
@@ -34,4 +42,22 @@ function draw() {
             square(x, y, w)
         }
     }
+
+    let newGrid = make2DArray(cols, rows)
+    for(let i = 0; i < cols; i++) {
+        for(let j = 0; j < rows; j++) {
+           let currentSand = grid[i][j]
+           if(currentSand > 0){
+             let belowSand = grid[i][j + 1]
+             if(belowSand === 0){
+                newGrid[i][j] = 0
+                newGrid[i][j + 1] = 1
+             }
+           }
+           
+        }
+
+    }
+    grid = newGrid
 }
+
