bc9abc192e048deea0d084ae8a86b6f23eb5a2c9
g0sh06 <g0sh06@github.com>
2024-04-13 15:41:18 +0200

    Finished Version 1

--- fallingSandSim.js
+++ fallingSandSim.js
@@ -1,6 +1,7 @@
 let grid;
 const w = 10;
 let cols, rows; // Declare cols and rows globally
+let hueValue = 10
 
 // making 2d array
 function make2DArray(cols, rows) {
@@ -26,23 +27,30 @@ function setup() {
 function mouseDragged() {
     let i = floor(mouseX / w);
     let j = floor(mouseY / w);
-    grid[i][j] = 1;
+    if(i >= 0 && 
+       i <= cols - 1 &&
+       j >= 0 &&
+       j <= rows - 1) {
+            grid[i][j] = hueValue;
+        }
+    hueValue += 0.5
 }
 
 function draw() {
+    background(0)
     // if the box is 1 it will be black
     // if the box is 0 it will be white
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) { // Corrected loop condition
-            stroke(255)
-            fill(grid[i][j] * 255);
-            let x = i * w
-            let y = j * w
-            square(x, y, w)
+            noStroke()
+            if(grid[i][j] > 0) {
+                fill(grid[i][j], 255, 255);
+                let x = i * w
+                let y = j * w
+                square(x, y, w)
+            }
         }
     }
-
-
 
     let newGrid = make2DArray(cols, rows)
     for (let i = 0; i < cols; i++) {
@@ -52,14 +60,16 @@ function draw() {
                 let belowSand = grid[i][j + 1]
                 if (belowSand === 0 && j < rows - 1) {
                     newGrid[i][j] = 0
-                    newGrid[i][j + 1] = 1
+                    newGrid[i][j + 1] = grid[i][j]
                 } else {
                     let direction = Math.random() < 0.5 ? -1 : 1
+                    // taking the main grid, because the newGrid is used only for making changes
+                    // and the main grid contains already declared changes which includes the 
                     if (i + direction >= 0 && i + direction < cols && grid[i + direction][j + 1] === 0) {
                         newGrid[i][j] = 0;
-                        newGrid[i + direction][j + 1] = 1;
+                        newGrid[i + direction][j + 1] = grid[i][j];
                     } else {
-                        newGrid[i][j] = 1
+                        newGrid[i][j] = grid[i][j]
                     }
                 }
             }
